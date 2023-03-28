1789720e9489a098cba2fdc3d012e1c0402ae610
andy <q25a25q@live.com>
2023-03-28 14:26:54 +0800

    localStorageMock

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -8,3 +8,31 @@ jest.mock('react', () => ({
   }));
   
   jest.setTimeout(60000);
+
+
+// browserMocks.js
+export const localStorageMock = (() => {
+  let store: any = {
+    umi_locale: 'zh-CN',
+  };
+
+  return {
+    getItem(key: string) {
+      return store[key] || null;
+    },
+    setItem(key: string, value: string) {
+      store[key] = value.toString();
+    },
+    removeItem(key: string) {
+      store[key] = null;
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+  writable: true,
+});
