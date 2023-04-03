a63bd9b56e02aa088234d82ad45c9ae5eafde932
andy <q25a25q@live.com>
2023-04-03 16:14:25 +0800

    bladeUserService：添加api

--- packages/blade/src/services/system/bladeUserService.ts
+++ packages/blade/src/services/system/bladeUserService.ts
@@ -36,11 +36,22 @@ export type BladeUser = {
 
 export type BladeUserService = {
   getUserInfo: () => Promise<BladeUser>;
+  // getList,
+  // remove,
+  // add,
+  // update,
+  // updatePlatform,
+  // getUser,
+  // getUserPlatform,
+  updatePassword: (
+    oldPassword: string,
+    newPassword: string,
+    newPassword1: string
+  ) => Promise<any>;
+  updateInfo: (bladeUser: BladeUser) => Promise<any>;
 };
 
-export const bladeUserService = ({
-  httpClient,
-}: ServiceParams): BladeUserService => {
+export const bladeUserService = ({ httpClient }: ServiceParams) => {
   // export const getList = (current, size, params, deptId) => {
   //   return request({
   //     url: '/api/blade-user/page',
@@ -118,24 +129,24 @@ export const bladeUserService = ({
   //     },
   //   });
   // };
-  // export const updatePassword = (oldPassword, newPassword, newPassword1) => {
-  //   return request({
-  //     url: '/api/blade-user/update-password',
-  //     method: 'post',
-  //     params: {
-  //       oldPassword,
-  //       newPassword,
-  //       newPassword1,
-  //     },
-  //   });
-  // };
-  // export const updateInfo = (row) => {
-  //   return request({
-  //     url: '/api/blade-user/update-info',
-  //     method: 'post',
-  //     data: row,
-  //   });
-  // };
+  const updatePassword = (
+    oldPassword: string,
+    newPassword: string,
+    newPassword1: string
+  ) => {
+    return httpClient.post("/api/blade-user/update-password", {
+      params: {
+        oldPassword,
+        newPassword,
+        newPassword1,
+      },
+    });
+  };
+  const updateInfo = (bladeUser: BladeUser) => {
+    return httpClient.post("/api/blade-user/update-info", {
+      data: bladeUser,
+    });
+  };
   // export const grant = (userIds, roleIds) => {
   //   return request({
   //     url: '/api/blade-user/grant',
@@ -184,5 +195,19 @@ export const bladeUserService = ({
   // };
   return {
     getUserInfo,
+    // getList,
+    // remove,
+    // add,
+    // update,
+    // updatePlatform,
+    // getUser,
+    // getUserPlatform,
+    updatePassword,
+    updateInfo,
+    // grant,
+    // unlock,
+    // retrieveCode,
+    // retrievePassword
+    // resetPassword
   };
 };
