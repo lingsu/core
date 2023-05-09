e8872200daf3c5f869981416f19310219c27febb
andy <q25a25q@live.com>
2023-05-09 15:49:24 +0800

    修复bug

--- packages/blade-react/src/hooks/index.ts
+++ packages/blade-react/src/hooks/index.ts
@@ -1,4 +1,11 @@
 import useDictRequestOptions from "./useDictRequestOptions";
 import useRegionRequestOptions from "./useRegionRequestOptions";
+import useDictValueEnum from "./useDictValueEnum";
+import useRegionValueEnum from "./useRegionValueEnum";
 
-export { useDictRequestOptions, useRegionRequestOptions };
+export {
+  useDictRequestOptions,
+  useRegionRequestOptions,
+  useDictValueEnum,
+  useRegionValueEnum,
+};
