1a684504c62a7eef7a5eea379ad1418c697654df
andy <q25a25q@live.com>
2023-05-09 15:54:40 +0800

    修复bug

--- packages/blade-react/src/hooks/useRegionRequestOptions.ts
+++ packages/blade-react/src/hooks/useRegionRequestOptions.ts
@@ -3,7 +3,7 @@ import { optionsConvert, OptionsConvertOption } from "@q25a25q/common";
 import { useEffect, useState } from "react";
 import { createApi } from "..";
 
-export default (code: string, dickOption?: OptionsConvertOption) => {
+export default (code?: string, dickOption?: OptionsConvertOption) => {
   const [dataSource, setDataSource] = useState<RequestOptionsType[]>(
     [] as RequestOptionsType[]
   );
--- packages/blade-react/src/hooks/useRegionValueEnum.ts
+++ packages/blade-react/src/hooks/useRegionValueEnum.ts
@@ -2,8 +2,8 @@ import { ProSchemaValueEnumObj } from "@ant-design/pro-components";
 import { OptionsConvertOption } from "@q25a25q/common";
 import { useRegionRequestOptions } from "..";
 
-export default (dictKey: string, dickOption?: OptionsConvertOption) => {
-  const dataSource = useRegionRequestOptions(dictKey, dickOption);
+export default (code?: string, dickOption?: OptionsConvertOption) => {
+  const dataSource = useRegionRequestOptions(code, dickOption);
 
   return dataSource.reduce((acc, value) => {
     return {
