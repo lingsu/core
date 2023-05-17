8d56ceb68c706be3185407fcb5afa76a32a65dd4
andy <q25a25q@live.com>
2023-05-17 10:47:11 +0800

    fix bugs

--- packages/common/src/converts/index.ts
+++ packages/common/src/converts/index.ts
@@ -2,17 +2,12 @@ import objectToPostParamConvert from "./objectToPostParamConvert";
 import stringToDateTimeConvert from "./stringToDateTimeConvert";
 import intOrStringArrayToStringConvert from "./intOrStringArrayToStringConvert";
 import stringToIntArrayConvert from "./stringToIntArrayConvert";
-import optionsConvert, {
-  Options,
-  OptionsConvertOption,
-} from "./optionsConvert";
-
+import optionsConvert from "./optionsConvert";
+export type { Options, OptionsConvertOption } from "./optionsConvert";
 export {
   objectToPostParamConvert,
   stringToDateTimeConvert,
   intOrStringArrayToStringConvert,
   optionsConvert,
-  Options,
-  OptionsConvertOption,
-  stringToIntArrayConvert
+  stringToIntArrayConvert,
 };
