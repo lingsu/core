efd5bdf4fdad60d1519bbbd9b3870aee7681282b
andy <570678569@qq.com>
2023-06-20 16:54:08 +0800

    添加导出

--- packages/blade/src/utils/index.ts
+++ packages/blade/src/utils/index.ts
@@ -1,5 +1,9 @@
 import toBladeBackEndObjectFormat from './toBladeBackEndObjectFormat';
+import convertBladeModel from './convertBladeModel';
+import guessType from './guessType';
 
 export {
     toBladeBackEndObjectFormat,
+    convertBladeModel,
+    guessType
 }
