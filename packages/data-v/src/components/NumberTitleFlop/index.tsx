928cbac036bf49fb4043bb0363558f2c90f02934
andy <570678569@qq.com>
2023-08-09 22:00:41 +0800

    test CountUp

--- packages/data-v/src/components/NumberTitleFlop/index.tsx
+++ packages/data-v/src/components/NumberTitleFlop/index.tsx
@@ -4,14 +4,42 @@ import { DatavComWrapperContext } from "../DatavComWrapper/context";
 import CountUp from "react-countup";
 import getTextStyle from "../../utils/getTextStyle";
 import { css } from "@emotion/css";
+import React from "react";
+import { CountUp as CountUpJs } from "countup.js";
 
 // {"main-img_4bzfk/source":{"type": "static", "isError":false,"data":[{"img":"","url":""}]},
 // "main-title_qeqfb/source":{"type": "static", "isError":false,"data":[]},
 // "number-title-flop_d0upt/source":{"type": "static", "isError":false,"data":[{"name":"","value":232425}]}}
-
+export class SomePlugin {
+  // ...some properties here
+  constructor(style: any) {
+    // ...setup code here if you need it
+    console.log("SomePlugin");
+  }
+  render(elem: HTMLElement, formatted: string): void {
+    // render DOM here
+    console.log("formatted", elem, formatted);
+    // elem.innerHTML =
+    var spans: string[] = [];
+    for (let i = 0; i < formatted.length; i++) {
+      const chat = formatted[i];
+      if (chat == ",") {
+        spans.push(`<span>${chat}</span>`);
+      } else {
+        spans.push(
+          `<span style="background:blue; border-radius: 10px">${chat}</span>`
+        );
+      }
+    }
+    elem.innerHTML = spans.join("");
+  }
+}
 export default (props: CommonWidgetProps) => {
   const { widget } = props;
   const [value, setValue] = useState(232425);
+
+  const containerRef = React.useRef<HTMLElement>(null);
+
   const { title, counter = {} as ICounter } = widget.props;
   const { numbers, prefix, suffix, fontFamily, justifyContent, margin } =
     counter;
@@ -26,6 +54,13 @@ export default (props: CommonWidgetProps) => {
   const prefixStyle = getTextStyle(prefix?.textStyle);
   const suffixStyle = getTextStyle(suffix?.textStyle);
   const numbersStyle = getTextStyle(numbers?.textStyle);
+
+  useEffect(() => {
+    const countUp = new CountUpJs(containerRef.current!, 5234, {
+      plugin: new SomePlugin(numbersStyle),
+    });
+    countUp.start();
+  }, []);
   return (
     <>
       {title && <div style={titleStyle}>{title.content}</div>}
@@ -48,6 +83,13 @@ export default (props: CommonWidgetProps) => {
           </span>
         )}
 
+        <span
+          ref={containerRef}
+          style={numbersStyle}
+          className={css`
+            letter-spacing: ${numbers.marginRight}em;
+          `}
+        ></span>
         <CountUp
           end={value}
           preserveValue={true}
@@ -60,6 +102,7 @@ export default (props: CommonWidgetProps) => {
           className={css`
             letter-spacing: ${numbers.marginRight}em;
           `}
+          plugin={new SomePlugin(numbersStyle)}
         />
         {suffix && (
           <span
