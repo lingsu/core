b6bac9b48dcb05234bdf76f00c49308760936cee
andy <570678569@qq.com>
2023-09-11 17:37:59 +0800

    add numToUpper

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -9,6 +9,7 @@ import getQueryString from "./getQueryString";
 import generateMD5Hash from "./generateMD5Hash";
 import log from "./log";
 import { buildUUID, buildShortUUID } from "./uuid";
+import numToUpper from './numToUpper'
 
 export {
   getFirstAndLastDay,
@@ -23,4 +24,5 @@ export {
   log,
   buildUUID,
   buildShortUUID,
+  numToUpper
 };
