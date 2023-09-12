802589f47a8d8fcfde243eb82999eab01985f293
andy <570678569@qq.com>
2023-09-12 09:51:57 +0800

    add mitt

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -9,7 +9,8 @@ import getQueryString from "./getQueryString";
 import generateMD5Hash from "./generateMD5Hash";
 import log from "./log";
 import { buildUUID, buildShortUUID } from "./uuid";
-import numToUpper from './numToUpper'
+import numToUpper from "./numToUpper";
+import mitt from "./mitt";
 
 export {
   getFirstAndLastDay,
@@ -24,5 +25,6 @@ export {
   log,
   buildUUID,
   buildShortUUID,
-  numToUpper
+  numToUpper,
+  mitt,
 };
