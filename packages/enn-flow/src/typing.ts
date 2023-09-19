8385818b01e8eff95ff45f5e20414c84e071b198
andy <570678569@qq.com>
2023-09-19 20:45:41 +0800

    add type

--- packages/enn-flow/src/typing.ts
+++ packages/enn-flow/src/typing.ts
@@ -1,6 +1,8 @@
 import { CSSProperties, ReactNode } from "react";
 import { internalsSymbol } from "./constants";
 
+
+
 export type Node<T = any> = {
   id: string;
   position: XYPosition;
@@ -36,6 +38,52 @@ export type Node<T = any> = {
     isParent?: boolean;
   };
 };
+
+
+export type NodeDimensionChange = {
+  id: string;
+  type: 'dimensions';
+  dimensions?: Dimensions;
+  updateStyle?: boolean;
+  resizing?: boolean;
+};
+
+export type NodePositionChange = {
+  id: string;
+  type: 'position';
+  position?: XYPosition;
+  positionAbsolute?: XYPosition;
+  dragging?: boolean;
+};
+
+export type NodeSelectionChange = {
+  id: string;
+  type: 'select';
+  selected: boolean;
+};
+
+export type NodeRemoveChange = {
+  id: string;
+  type: 'remove';
+};
+
+export type NodeAddChange<NodeData = any> = {
+  item: Node<NodeData>;
+  type: 'add';
+};
+
+export type NodeResetChange<NodeData = any> = {
+  item: Node<NodeData>;
+  type: 'reset';
+};
+
+export type NodeChange =
+  | NodeDimensionChange
+  | NodePositionChange
+  | NodeSelectionChange
+  | NodeRemoveChange
+  | NodeAddChange
+  | NodeResetChange;
 
 
 type EdgeLabelOptions = {
@@ -135,3 +183,16 @@ export interface Box extends XYPosition {
 export type Transform = [number, number, number];
 
 export type CoordinateExtent = [[number, number], [number, number]];
+
+
+export type EdgeSelectionChange = NodeSelectionChange;
+export type EdgeRemoveChange = NodeRemoveChange;
+export type EdgeAddChange<EdgeData = any> = {
+  item: Edge<EdgeData>;
+  type: 'add';
+};
+export type EdgeResetChange<EdgeData = any> = {
+  item: Edge<EdgeData>;
+  type: 'reset';
+};
+export type EdgeChange = EdgeSelectionChange | EdgeRemoveChange | EdgeAddChange | EdgeResetChange;
--- packages/enn-flow/src/utils/changes.ts
+++ packages/enn-flow/src/utils/changes.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Node, Edge, EdgeChange, NodeChange } from '../typing';
 
 function handleParentExpand(res: any[], updateItem: any) {
