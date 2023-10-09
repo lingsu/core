645baff2715cf4a21f329e23e325ef62c47a436a
andy <570678569@qq.com>
2023-10-09 09:21:08 +0800

    add missed export

--- packages/data-v/src/index.ts
+++ packages/data-v/src/index.ts
@@ -2,6 +2,7 @@ export { default as Stage } from "./components/Stage";
 export { default as AbsoluteContainer } from "./components/AbsoluteContainer";
 export { default as MainColorBlock } from "./components/MainColorBlock";
 export { default as MainTitle } from "./components/MainTitle";
+export { default as MainImage } from "./components/MainImage";
 export { default as NumberTitleFlop } from "./components/NumberTitleFlop";
 
 
