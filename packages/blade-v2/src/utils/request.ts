760a1e1527ea9c59bd66fc733062ee408c00a36a
andy <570678569@qq.com>
2023-11-15 10:16:17 +0800

    clear code

--- packages/blade-v2/src/utils/request.ts
+++ packages/blade-v2/src/utils/request.ts
@@ -1,6 +1,5 @@
 import { message } from "antd";
 import downloadByUrl from "./downloadByUrl";
-import { RequestData } from "@ant-design/pro-components";
 import { getToken, getTokenHeader } from "./auth";
 import { AntdPage, BladeRequest, PageResult, RequestOption, RequestParams } from "../typing";
 
